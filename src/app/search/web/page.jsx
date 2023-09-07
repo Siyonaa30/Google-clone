278aebd3b72b008d8b29190635c9b3be134b141e
faatima abdirahmaan ali <faatimaabdirahmaanali@gmail.com>
2023-09-07 09:03:41 +0300

    handeling errors

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,8 +1,24 @@
+import Link from "next/link";
+
 export default async function WebSearchPage({ searchParams }) {
   const res = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
   const data = await res.json();
   const results = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">no results found</h1>
+        <p className="text-lg">
+          Try search something else or go back to home page{"    "}
+          <Link className="text-blue-500" href="/">
+            Home.
+          </Link>
+        </p>
+      </div>
+    );
+  }
   return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 }
