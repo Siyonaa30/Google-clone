92916c0d232092cd9729991b72ee49e5d8a8dc3f
faatima abdirahmaan ali <faatimaabdirahmaanali@gmail.com>
2023-09-08 12:39:31 +0300

    create search result component

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,6 +1,9 @@
+import WebSearchResults from "@/components/WebSearchResults";
 import Link from "next/link";
+import { resolve } from "styled-jsx/css";
 
 export default async function WebSearchPage({ searchParams }) {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
   const res = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
@@ -20,5 +23,5 @@ export default async function WebSearchPage({ searchParams }) {
       </div>
     );
   }
-  return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
+  return <>{results && <WebSearchResults results={data} />}</>;
 }
